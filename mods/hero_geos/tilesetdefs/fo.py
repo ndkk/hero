aa55f07716005290110fbcb2417c0a4060b27cdb
ndkk <ThreePrinciple@outlook.com>
2023-06-16 16:06:35 +0800

    solve hero hidden in a house.

--- mods/hero_geos/tilesetdefs/fo.py
+++ mods/hero_geos/tilesetdefs/fo.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 # 打开文件
-fo = open("maps.txt", "r+")
+fo = open("maps_tall.txt", "r+")
 print ("文件名为: ", fo.name)
-dstfo = open("city_dst.txt", "w+")
+dstfo = open("maps_tall_dst.txt", "w+")
 
 for line in fo:
     linedst = line
@@ -12,7 +12,7 @@ for line in fo:
         linearr1 = line.split("=")
         linearr2 = linearr1[1].split(",")
         dstno = linearr2[0]
-        dstno = int(dstno) + 10
+        dstno = int(dstno) + 1001 - 1
         linearr2[0] = str(dstno)
         linedst = "tile=" + ','.join(linearr2)
     dstfo.write(linedst)
