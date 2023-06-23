fab3e4efcfca500524484d9a04ec460908f75a03
ndkk <ThreePrinciple@outlook.com>
2023-06-23 23:19:36 +0800

    make map_tall.png to hidden hero

---
+++ tiled/geos/map_tall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="map_tall" tilewidth="32" tileheight="128" tilecount="60" columns="20">
+ <image source="../../art_src/map_tall.png" width="640" height="480"/>
+</tileset>
