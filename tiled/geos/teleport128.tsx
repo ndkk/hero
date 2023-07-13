323b7be323a286a94e9ed91ac7be8e30bebda0bf
ndkk <ThreePrinciple@outlook.com>
2023-07-13 19:56:48 +0800

    add many scenes include seafoodmarket beijingcdc

---
+++ tiled/geos/teleport128.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="teleport128" tilewidth="128" tileheight="64" tilecount="5" columns="1">
+ <grid orientation="orthogonal" width="128" height="128"/>
+ <image source="../../art_src/teleport_128.png" width="128" height="320"/>
+</tileset>
